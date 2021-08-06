9ec77e0f9e4d1690a6ae6809c6af439d1e16d4df
leonardo gomes <leonardogomespadilha@gmail.com>
2021-08-06 13:13:37 -0300

    Exercicio soma dobrada

--- src/implementacao/logica/soma-dobrada.js
+++ src/implementacao/logica/soma-dobrada.js
@@ -1,4 +1,12 @@
 function somaDobrada(umValor, outroValor) {
-    return null;
+    if(umValor === outroValor){
+        var soma = umValor + outroValor;
+        return soma * 2;
+    }
+    else if(umValor <1 || outroValor <1){
+        return -1
+    }else{
+        return umValor + outroValor;
+    }
 }
 module.exports = somaDobrada;
