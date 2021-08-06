9fb94ecabb057b7640f7588344d5632ee9cf349f
Erick Cardoso <cyber666king@gmail.com>
2021-08-06 11:12:41 -0300

    Continua exercicio Fizz Buzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,12 +1,12 @@
 function fizzBuzz(numero) {
-    if (numero % 3 == 0){
-        return "Fizz"
+    if (numero % 3 == 0 && numero % 5 == 0){
+        return "FizzBuzz"
     }
     else if(numero % 5 == 0){
         return "Buzz"
     }
-    else if(numero % 3 == 0 && numero % 5 == 0){
-        return "FizzBuzz"
+    else if(numero % 3 == 0){
+        return "Fizz"
     }
     else {
         return String(numero)
