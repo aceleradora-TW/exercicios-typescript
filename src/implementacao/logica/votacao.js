fe6f6176ee349b06c781d5b2458d2b12ae6381fd
nicoledbianchin <nicoledbianchin@gmail.com>
2021-08-09 11:37:24 -0300

    implementa votacao simples

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,35 @@
 function apuraVotacoa(votos) {
-    return null;
+
+    // switch case
+    var contador1 = 0
+    var contador2 = 0
+    var contador3 = 0
+
+    // votos.forEach(voto => {
+    //     if(voto === 1){
+    //     }
+    // });
+
+
+    for (var i = 0; i <= votos.length; i++) {
+        if (votos[i] === 1) {
+            contador1++;
+        } else if (votos[i] === 2) {
+            contador2++;
+        } else if (votos[i] === 3) {
+            contador3++;
+        }
+    }
+
+    if (contador1 > contador2 && contador1 > contador3) {
+        return 'Vencedor: 1';
+    } else if (contador2 > contador1 && contador2 > contador3) {
+        return 'Vencedor: 2';
+    } else if (contador3 > contador1 && contador3 > contador2) {
+        return 'Vencedor: 3';
+    } else if (contador1 === contador2 || contador2 === contador3 || contador1 === contador3) {
+        return 'Empate';
+    }
+
 }
 module.exports = apuraVotacoa;
