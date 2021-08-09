a13e467d57ff3582936807f0a5a55c523db5e5a6
leonardo gomes <leonardogomespadilha@gmail.com>
2021-08-09 15:54:41 -0300

    simplificação exercicio votacao

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,42 +1,40 @@
 function apuraVotacoa(votos) {
 
     // switch case
-    var contador1 = 0
-    var contador2 = 0
-    var contador3 = 0
+    let contador1 = 0
+    let contador2 = 0
+    let contador3 = 0
 
-     votos.forEach(voto => {
-         if(voto === 1){
-             contador1++;
-         }
-         if(voto === 2){
-            contador2++;
-        }
-        if(voto === 3){
-            contador3++;
+     votos.forEach(voto => {        
+        switch(voto) {
+            case 1:
+                contador1++
+                break;
+                
+            case 2:
+                contador2++
+                break;
+            
+            case 3:
+                contador3++
+                break;
         }
      });
 
 
-    /*for (var i = 0; i <= votos.length; i++) {
-        if (votos[i] === 1) {
-            contador1++;
-        } else if (votos[i] === 2) {
-            contador2++;
-        } else if (votos[i] === 3) {
-            contador3++;
-        }
-    }*/
-
-    if (contador1 > contador2 && contador1 > contador3) {
-        return 'Vencedor: 1';
-    } else if (contador2 > contador1 && contador2 > contador3) {
-        return 'Vencedor: 2';
-    } else if (contador3 > contador1 && contador3 > contador2) {
-        return 'Vencedor: 3';
-    } else if (contador1 === contador2 || contador2 === contador3 || contador1 === contador3) {
-        return 'Empate';
+     if (contador1 > contador2 && contador1 > contador3) {
+        return 'Vencedor: 1'
     }
+
+    if (contador2 > contador1 && contador2 > contador3) {
+        return 'Vencedor: 2'
+    }
+    
+    if (contador3 > contador1 && contador3 > contador2) {
+        return 'Vencedor: 3'
+    }
+
+    return 'Empate'
 
 }
 module.exports = apuraVotacoa;
