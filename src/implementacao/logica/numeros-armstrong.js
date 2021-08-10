e473c17606368b712e5a8974be55ef5df411e51f
luiz Henrique <luizlhra24@gmail.com>
2021-08-10 13:26:45 -0300

    exercícos Armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,20 @@
 function numerosArmstrong(numero) {
-    return null;
+
+
+    let digitos = numero.toString().split('')
+    let digitosReais = numero
+    let a = 0
+
+    for (let i = 0; i < digitos.length; i++) {
+        digitos[i] = digitos[i] ** digitos.length
+        a += digitos[i]
+    }
+
+    if (a == digitosReais) {
+        return "Este eh um numero de Armstrong!";
+    } else {
+        return "Este nao eh um numero de Armstrong!";
+    }
+
 }
 module.exports = numerosArmstrong;
