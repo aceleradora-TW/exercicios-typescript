ddb2abfbc87a98debbb582e604d4806344aaec16
Bianca Salino <biancadsalino@gmail.com>
2021-08-25 13:54:15 -0300

    inicia exercicio FizzBuzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,4 +1,16 @@
 function fizzBuzz(numero) {
-    return null;
-}
-module.exports = fizzBuzz;
+    //3, 5, 3 && 5 else - nenhum
+
+
+
+    if( numero % 3 == 0 ){
+
+        return 'Fizz' 
+
+    }else if (numero % 5 == 0){
+        return'Buzz' 
+    }else if (numero % 3 == 0 && numero % 5 == 3 ){
+        return 'FizzBuzz'
+    }else{
+        return numero
+    }
