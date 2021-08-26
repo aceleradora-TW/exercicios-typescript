d038dd78e9d0c45f24d160c70136fecb096d783b
patricia <patriciatersariol@gmail.com>
2021-08-26 11:54:34 -0300

    exercicio-armstrong-finalizado

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,19 @@
 function numerosArmstrong(numero) {
-    return null;
+  let digitosArray = numero.toString().split("");
+
+  let quantidadeDeDigitos = digitosArray.length;
+
+  let digitosNumeros = digitosArray.map((digito) => parseInt(digito));
+
+  let resultado = 0;
+  for (let i = 0; i < quantidadeDeDigitos; i++) {
+    resultado += Math.pow(digitosNumeros[i], quantidadeDeDigitos);
+  }
+
+  if (resultado == numero) {
+    return "Este eh um numero de Armstrong!";
+  } else {
+    return "Este nao eh um numero de Armstrong!";
+  }
 }
 module.exports = numerosArmstrong;
