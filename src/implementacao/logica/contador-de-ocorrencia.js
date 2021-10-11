ab058f5f7d1884c2c6051cef93baee3151b7eb22
inglyd <inglydalves@outlook.com>
2021-10-11 10:40:59 -0300

    exercicio contador-de-ocorrencia

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,4 +1,11 @@
 function contadorDeOcorrencia(texto, letra) {
-    return null;
+  
+    let contador = 0;
+
+   for (let i = 0; i < texto.length; i++) {
+       if(texto[i] == letra)  {
+           contador++
+       }
+   } return contador
 }
 module.exports = contadorDeOcorrencia;
