ab4fda9670dc8ef6fa7da0650fe0afa40ae3c83b
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-10-11 11:10:33 -0300

    Exercício objeto-data

--- src/implementacao/logica/objeto-data.js
+++ src/implementacao/logica/objeto-data.js
@@ -1,4 +1,11 @@
 function criaObjeto(data) {
-    return null;
+    let lista = data.split("/")
+
+    let objeto = {
+        dia: lista[0],
+        mes: lista[1],
+        ano: lista[2]
+    }
+    return objeto;
 }
 module.exports = criaObjeto;
