63f398768544088c28a7164c969826d79ff4f19a
Carolina Borges <carolinaborgescardoso@gmail.com>
2021-10-13 09:54:57 -0300

    numeros-armstrong concluído

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,30 +1,27 @@
 function numerosArmstrong(numero) {
-    //1 - mapear os dígitos
-    //2 - mapear a quant de dígitos = transformar em string e depois fazer o split
-    //3 - (dígito1)^quant + (dígito2)^quant + (dígito3)^quant = result
-    //se numero === result "é num de armstrong"
-    //se não "n é num de armstrong"
 
-    let result = "0";
-    let numString = numero.toString();
-    let quantDigitos = numString.split("")
-    let potencia = quantDigitos.length
-    let num = quantDigitos.map((digito) => parseInt(digito))
+    let result;
+    let resultado = 0;
 
-    for (let i = 0; i < potencia; i++) {
-        quantDigitos[i] = (quantDigitos[i] ** potencia)
-        //153 = 1 ^ 3 = 1
-        //153 = 5 ^ 3 = 125 
-        //153 = 3 ^ 3 = 27
-        result += quantDigitos[i]
-        //result deve ser cada dígito elevado ao tamanho do array (quant de dígitos)
+    let numParaString = String(numero)
+
+    let arrayDigitos = numParaString.split("")
+
+    let num = arrayDigitos.map((digito) => parseInt(digito))
+
+    for (var i = 0; i < arrayDigitos.length; i++) {
+
+        result = Math.pow(num[i], arrayDigitos.length)
+        resultado += result
     }
 
-    if (result == numero) {
+
+    if (resultado == numero) {
         return "Este eh um numero de Armstrong!"
     } else {
         return "Este nao eh um numero de Armstrong!"
     }
+
 
 }
 module.exports = numerosArmstrong;
