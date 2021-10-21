1127e92bca7dd9f90dd677e3cfc880ad9b88e3a7
Cibely Pereira <cibely.port@gmail.com>
2021-10-21 11:07:39 -0300

    finaliza soma dobrada

--- src/implementacao/logica/soma-dobrada.js
+++ src/implementacao/logica/soma-dobrada.js
@@ -1,4 +1,13 @@
 function somaDobrada(umValor, outroValor) {
-    return null;
+    if(umValor == outroValor) {
+        const soma = umValor + outroValor 
+        return soma * 2 
+    }
+    else if (umValor <1 || outroValor <1) {
+        return -1
+    }
+    else {
+        return umValor + outroValor
+    }
 }
 module.exports = somaDobrada;
