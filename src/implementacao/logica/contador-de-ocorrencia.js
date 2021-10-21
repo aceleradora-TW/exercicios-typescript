2522d3bd324592356c06a6935749e375c035eb84
Cibely Pereira <cibely.port@gmail.com>
2021-10-21 13:57:27 -0300

    finaliza contador de ocorrencia

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,4 +1,13 @@
 function contadorDeOcorrencia(texto, letra) {
-    return null;
+    
+    let contador = 0
+    let str = texto
+    let posicao = str.indexOf(letra)
+
+    while (posicao != -1) {
+        contador++ 
+        posicao = str.indexOf(letra, posicao++)
+    }
+    return contador 
 }
 module.exports = contadorDeOcorrencia;
