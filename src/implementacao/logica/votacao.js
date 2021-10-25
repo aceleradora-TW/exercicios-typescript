58158c280aa63d2ee03ce9540209276defef3c9f
Cibely Pereira <cibely.port@gmail.com>
2021-10-25 11:46:00 -0300

    Finalizado Votacao

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,34 @@
 function apuraVotacoa(votos) {
-    return null;
+    let contador1 = 0
+    let contador2 = 0
+    let contador3 = 0
+
+    votos.forEach(voto => {
+     switch (voto) {
+         case 1:
+           contador1++  
+             break;
+
+             case 2:
+                 contador2++
+                 break;
+
+                 case 3:
+                     contador3++
+                     break;
+     }   
+    });
+
+    if (contador1 > contador2 && contador1 > contador3){ 
+        return "Vencedor: 1"
+    }
+    if (contador2 > contador1 && contador2 > contador3){
+        return "Vencedor: 2"
+    }
+    if (contador3 > contador2 && contador3 > contador1){
+        return "Vencedor: 3"
+    }
+    else {    return "Empate"
 }
+ }
 module.exports = apuraVotacoa;
