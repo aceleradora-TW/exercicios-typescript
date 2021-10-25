e48f7aa537f3b8c69930b7590f2cf25a03201827
Cibely Pereira <cibely.port@gmail.com>
2021-10-25 13:57:07 -0300

    finalizando trilha

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,22 @@
 function numerosArmstrong(numero) {
-    return null;
+    // 153 = precisamos saber quantos caracteres tem neste numero.
+    // toString()
+    // contador tem que começar em 0 --- EXEMPLO => let contador = 0
+    // 'Este eh um numero de Armstrong!' ----- 'Este nao eh um numero de Armstrong!
+
+    const numeroDeDigitos = numero.toString().split("")
+    let contador = 0 
+
+    for (let i = 0; i < numeroDeDigitos.length; i++){
+        numeroDeDigitos[i] = numeroDeDigitos[i] ** numeroDeDigitos.length
+        contador += numeroDeDigitos[i]
+    }
+
+    if (contador == numero) {
+        return "Este eh um numero de Armstrong!"
+    } else {
+        return "Este nao eh um numero de Armstrong!"
+        
+    }
 }
 module.exports = numerosArmstrong;
