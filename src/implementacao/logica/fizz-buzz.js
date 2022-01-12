fd1498d777c6e180185b6e080f390a1de1b676c9
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-12 10:48:03 -0300

    Finalizada atividade FizzBuzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,7 +1,15 @@
 function fizzBuzz(numero) {
-    if (numero % 3 ===0){
-    return "Fizz"
-    }  
-
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        return "FizzBuzz"
+    }
+    if (numero % 3 === 0) {
+        return "Fizz"
+    }
+    if (numero % 5 === 0) {
+        return "Buzz"
+    }
+    else {
+        return numero.toString()
+    }
 }
 module.exports = fizzBuzz;
