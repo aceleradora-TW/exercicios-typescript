e8c5a2569e710345858c003e3616f2557d7fd37d
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-13 15:57:28 -0300

    Realizando atividade contador de ocorrencia

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,4 +1,18 @@
 function contadorDeOcorrencia(texto, letra) {
-    return null;
+    let contador = 0
+    let posicao = texto.indexOf(letra)
+    while (posicao != -1) {
+        console.log(contador);
+    }
 }
 module.exports = contadorDeOcorrencia;
+/*    
+    do {
+        texto + 1
+    }
+    while (
+        texto < 3
+    )
+    return letra
+}
+*/
