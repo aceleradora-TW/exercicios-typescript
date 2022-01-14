06aa6df6b3305f0288d505dcef6ae4a4c7527aa8
duda sadino <dudababy27@gmail.com>
2022-01-14 15:56:20 -0300

    exercicio contador de ocorrencia em andamento

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,18 +1,39 @@
 function contadorDeOcorrencia(texto, letra) {
     let contador = 0
     let posicao = texto.indexOf(letra)
-    while (posicao != -1) {
+
+    for (let contador = 0; contador < 9; contador++) {
         console.log(contador);
     }
+
+    return contador
 }
 module.exports = contadorDeOcorrencia;
-/*    
-    do {
-        texto + 1
+
+/*
+
+   let contador = 0;
+    for (let i = 0; i < texto, letra; i++) {
+        console.log[i];
     }
-    while (
-        texto < 3
-    )
-    return letra
+
+let contador = 0
+let posicao = texto.indexOf(letra)
+do {
+    contador + 1
+
+}
+while (posicao != -1);
+return contador
+}
+console.log(contadorDeOcorrencia("bolo", "o"))
+module.exports = contadorDeOcorrencia;    
+do {
+    texto + 1
+}
+while (
+    texto < 3
+)
+return letra
 }
 */
