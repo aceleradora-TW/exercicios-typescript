747cf6773814afdcd9ed668b677df07547598112
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-17 14:08:39 -0300

    Resolução do exercício contador de ocorrencia

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,39 +1,11 @@
 function contadorDeOcorrencia(texto, letra) {
-    let contador = 1000
-    //let posicao = texto.indexOf(letra)
+    let contador = 0
     for (let i = 0; i < texto.length; i++) {
-        console.log(texto[i] + " - " + i);
+        if (texto[i] === letra) {
+            contador++
+        }
     }
 
     return contador
 }
-console.log(contadorDeOcorrencia("batata", "a"))
 module.exports = contadorDeOcorrencia;
-
-/*
- // console.log(posicao)
-   let contador = 0;
-    for (let i = 0; i < texto, letra; i++) {
-        console.log[i];
-    }
-
-let contador = 0
-let posicao = texto.indexOf(letra)
-do {
-    contador + 1
-
-}
-while (posicao != -1);
-return contador
-}
-console.log(contadorDeOcorrencia("bolo", "o"))
-module.exports = contadorDeOcorrencia;    
-do {
-    texto + 1
-}
-while (
-    texto < 3
-)
-return letra
-}
-*/
