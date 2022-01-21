82300d54c22636ff6403a7b480f94e7cd18b9730
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-21 15:01:02 -0300

    exercicio votação em andamento

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,35 @@
 function apuraVotacoa(votos) {
-    return null;
+    switch (votos) {
+        case "Vencedor 1":
+            console.log("qualquer coisa");
+            break;
+    }
 }
 module.exports = apuraVotacoa;
+
+/*
+
+while/doWhile =
+
+if/else =
+
+let jacó = "pastor"
+ if (jacó === "pastor")
+ return "jesus voltará"
+} else { "diabo toma conta"
+
+switchCase = 
+
+let votos = (numero do canditado)
+ = fazer alguma coisa
+switch (fruta){
+    case "Laranja":
+        console.log("as laranjas estão caras");
+break;
+     case "Maçã":
+        console.log("as maçãs estão baratas");
+} default: console.log("não temos laranjas nem maçãs" + fruta + ".")
+
+for = 
+
+*/
