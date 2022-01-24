ff7c0d48082fb8b5b3202c289ae3408eb506e3d9
Emilly marques <emillymarques96622@gmail.com>
2022-01-24 14:59:20 -0300

    exercicio de votação em andamento

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,10 +1,8 @@
-function apuraVotacoa(votos) {
+function apuraVotacao(votos) {
     let candidato1 = 0
     let candidato2 = 0
     let candidato3 = 0
-
-    for (let i = 0; i < votos.lenght; i++) {
-        console.log(votos[i])
+    for (let i = 0; i < votos.length; i++) {
         if (votos[i] === 1) {
             candidato1++
         }
@@ -14,12 +12,21 @@ function apuraVotacoa(votos) {
         else if (votos[i] === 3) {
             candidato3++
         }
-        console.log(candidato1)
-    }
 
+    }
+    if (candidato1 > candidato2 && candidato1 > candidato3) {
+        return "Vencedor:1"
+    }
+    if (candidato2 > candidato1 && candidato2 > candidato3) {
+        return "Vencedor:2"
+    }
+    if (candidato3 > candidato1 && candidato3 > candidato2) {
+        return "Vencedor:3"
+    }
 }
-apuraVotacoa([1, 1, 1, 2, 3, 3, 2])
-module.exports = apuraVotacoa;
+
+//(apuraVotacao([1, 1, 1, 2, 3, 2]))
+module.exports = apuraVotacao;
 
 /*
 
