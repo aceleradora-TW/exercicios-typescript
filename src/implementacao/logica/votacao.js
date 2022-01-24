a0ed92467a456e29c7aab07f9f8881c6eb5b393a
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-24 15:43:17 -0300

    Exercício votação concluido

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -15,52 +15,14 @@ function apuraVotacao(votos) {
 
     }
     if (candidato1 > candidato2 && candidato1 > candidato3) {
-        return "Vencedor:1"
+        return "Vencedor: 1"
     }
     if (candidato2 > candidato1 && candidato2 > candidato3) {
-        return "Vencedor:2"
+        return "Vencedor: 2"
     }
     if (candidato3 > candidato1 && candidato3 > candidato2) {
-        return "Vencedor:3"
+        return "Vencedor: 3"
     }
+    return "Empate"
 }
-
-//(apuraVotacao([1, 1, 1, 2, 3, 2]))
 module.exports = apuraVotacao;
-
-/*
-
-
-console.log(votos)
-    switch (votos) {
-        case "1": 
-    console.log("vencedor 1")
-            break;//
-    }
-
-
-
-while/doWhile =
-
-if/else =
-
-let jacó = "pastor"
- if (jacó === "pastor")
- return "jesus voltará"
-} else { "diabo toma conta"
-
-switchCase = 
-
-let votos = (numero do canditado)
- = fazer alguma coisa
-switch (fruta){
-    case "Laranja":
-        console.log("as laranjas estão caras");
-break;
-     case "Maçã":
-        console.log("as maçãs estão baratas");
-} default: console.log("não temos laranjas nem maçãs" + fruta + ".")
-
-for = 
-
-*/
