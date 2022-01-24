a42e68dd025179166d0904fdb194b907eda9945c
duda sadino <dudababy27@gmail.com>
2022-01-24 10:52:10 -0300

    exercicio apura votação em andamento

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,13 +1,37 @@
 function apuraVotacoa(votos) {
-    switch (votos) {
-        case "Vencedor 1":
-            console.log("qualquer coisa");
-            break;
+    let candidato1 = 0
+    let candidato2 = 0
+    let candidato3 = 0
+
+    for (let i = 0; i < votos.lenght; i++) {
+        console.log(votos[i])
+        if (votos[i] === 1) {
+            candidato1++
+        }
+        if (votos[i] === 2) {
+            candidato2++
+        }
+        else if (votos[i] === 3) {
+            candidato3++
+        }
+        console.log(candidato1)
     }
+
 }
+apuraVotacoa([1, 1, 1, 2, 3, 3, 2])
 module.exports = apuraVotacoa;
 
 /*
+
+
+console.log(votos)
+    switch (votos) {
+        case "1": 
+    console.log("vencedor 1")
+            break;//
+    }
+
+
 
 while/doWhile =
 
