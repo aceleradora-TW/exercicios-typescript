cfb1b25630de40edf2c9d52e1700fa49bf10375b
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-25 16:00:16 -0300

    realizando exercicio numeros de armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,30 @@
 function numerosArmstrong(numero) {
-    return null;
+
+    let numeros = [numero].toString()
+    let aleatoria = 0
+    
+    for (let i = 0; i < numeros.length ; i++){
+
+    }
+    return numeros
 }
+console.log(numerosArmstrong(153))
 module.exports = numerosArmstrong;
+
+// testar com for depois.
+
+    
+    // let soma = 0;
+
+    // let temporaria = numero;
+    // while (temporaria > 0) {
+    //     let resto = temporaria % 10;
+
+    //     soma += resto * resto * resto;
+
+    //     temporaria = parseInt(temporaria / 10);
+    // }
+    // if (soma == numero) {
+    //     return 'Este eh um numero de Armstrong!';
+    // }
+    // return 'Este nao eh um numero de Armstrong!';
