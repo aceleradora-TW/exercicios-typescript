d0962ba34a4c21fe5689d46e4e4c696b6a386145
Renato Souza <renatoosoouzaa26@gmail.com>
2022-01-27 15:52:56 -0300

    finalização do exercicio numeros de armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,32 +1,15 @@
 function numerosArmstrong(numero) {
 
     let numeros = numero.toString().split('')
-    let resultado = 0
-
+    let multiplicacao = 0
+    let soma = 0
     for (let i = 0; i < numeros.length; i++) {
-        resultado += numeros[i] ** numeros.length
-
-    } if (resultado === numero) {
+        multiplicacao = numeros[i] ** numeros.length
+        soma += multiplicacao
+    } if (soma == numero) {
         return "Este eh um numero de Armstrong!"
+    } else {
+        return "Este nao eh um numero de Armstrong!"
     }
-    return "Este nao eh um numero de Armstrong!"
 }
-console.log(numerosArmstrong(153))
 module.exports = numerosArmstrong;
-
-
-    // let soma = 0;
-
-    // let temporaria = numero;
-    // while (temporaria > 0) {
-    //     let resto = temporaria % 10;
-
-    //     soma += resto * resto * resto;
-
-    //     temporaria = parseInt(temporaria / 10);
-    // }
-  
-    // if (soma == numero) {
-    //     return 'Este eh um numero de Armstrong!';
-    // }
-    // return 'Este nao eh um numero de Armstrong!';
