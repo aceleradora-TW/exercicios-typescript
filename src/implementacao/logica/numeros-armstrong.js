53389b0ed5c92009e3ceb96fd2980b2667b46ee3
Emilly marques <emillymarques96622@gmail.com>
2022-01-28 11:07:21 -0300

    tarefa numero de armstrong concluida

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,20 +1,39 @@
 function numerosArmstrong(numero) {
+
+    let numeros = numero.toString().split('')
+    let resultado = 0
+
+    for (let i = 0; i < numeros.length ; i++){ 
+        var potencia = numeros[i] ** numeros.length
+        resultado += potencia
+    }
+    if(resultado === numero){
+        return 'Este eh um numero de Armstrong!'
+    }
+    
+    return 'Este nao eh um numero de Armstrong!'
+
+}
+//console.log(numerosArmstrong(54))
+module.exports=numerosArmstrong;
+
+
+/*function numerosArmstrong(numero) {
    
     let numeros = numero.toString().split('')
     let resultado = 0
 
     for (let i = 0; i < numeros.length ; i++){ 
         var potencia = numeros[i] ** numeros.length
-        if(resultado += potencia){ //resultado = resultado + potencia
-            return 'Este eh um numero de Armstrong!'
-            }
-            else if (resultado -= potencia){
-        return 'Este nao eh um numero de Armstrong!'
-            }
-     } 
+     resultado += potencia //resultado = resultado + potencia
+     
+     }
+     return resultado
 }
-
+console.log(numerosArmstrong(153))
 module.exports=numerosArmstrong;
+*/
+
 
 /*var potencia = numeros[i] ** numeros.length
 if(resultado += potencia){ //resultado = resultado + potencia
@@ -58,3 +77,52 @@ return 'Este nao eh um numero de Armstrong!'
     //     return 'Este eh um numero de Armstrong!';
     // }
     // return 'Este nao eh um numero de Armstrong!';
+
+
+
+
+/*
+    let resultados = 0
+    let numeros = numero.toString().split('')
+    //let aleatoria = 0//
+    
+    for (let i = 0; i < numeros.length ; i++){ 
+        console.log( numeros[i] **numeros.length ===resultados + resultados )
+
+*/
+
+// console.log( i + ' -------- ' + numeros[i] + ' ------' + **numeros.length )
+    
+    // let soma = 0;
+
+    // let temporaria = numero;
+    // while (temporaria > 0) {
+    //     let resto = temporaria % 10;
+
+    //     soma += resto * resto * resto;
+
+    //     temporaria = parseInt(temporaria / 10);
+    // }
+    // if (soma == numero) {
+    //     return 'Este eh um numero de Armstrong!';
+    // }
+    // return 'Este nao eh um numero de Armstrong!';
+
+/*sexta-feira
+
+    if(resultado -= potencia){ //resultado = resultado + potencia
+        console.log('Este eh um numero de Armstrong!')
+       }
+       else if (resultado += potencia){
+   console.log('Este nao eh um numero de Armstrong!')
+       }
+}
+return  resultado       if(resultado -= potencia){ //resultado = resultado + potencia
+    console.log('Este eh um numero de Armstrong!')
+   }
+   else if (resultado += potencia){
+console.log('Este nao eh um numero de Armstrong!')
+   }
+}
+return  resultado
+*/
