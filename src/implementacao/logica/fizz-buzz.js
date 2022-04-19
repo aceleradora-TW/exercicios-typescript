80c6b79e42c395c954a85aaf46adc461a113567b
Andressa Barcelos <andressa.barcelos26@gmail.com>
2022-04-19 14:03:33 -0300

    Adiciona inicio do exercicio Fizz Buzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,4 +1,12 @@
+
 function fizzBuzz(numero) {
-    return null;
+    
+    if (numero % 3 === 0){
+        return "fizz"
+    }
+    if(numero % 5 === 0){
+            
+    }
+    
 }
 module.exports = fizzBuzz;
