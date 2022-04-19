7c2b641b82aa37d933d10daf7bd74084e1fd78db
cintia <cintia.silvadeooliveira@gmail.com>
2022-04-19 15:44:05 -0300

    resolusão final fizzbuzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,12 +1,21 @@
-
 function fizzBuzz(numero) {
     
+    if (numero % 3 === 0 && numero % 5 === 0){
+        return "FizzBuzz";
+    }
     if (numero % 3 === 0){
-        return "fizz"
+        return "Fizz";
     }
     if(numero % 5 === 0){
-            
+       return "Buzz" ;    
+    }
+    else {
+        const numeroString = String(numero)
+        console.log (numeroString)
+        return numeroString
+        
     }
     
 }
+console.log(fizzBuzz(11))
 module.exports = fizzBuzz;
