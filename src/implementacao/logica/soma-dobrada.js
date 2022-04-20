6faf7dc3f5b0ba84608c969324c6ced60b18b7e2
Leticia <leticianunesq8@gmail.com>
2022-04-20 10:03:41 -0300

    Resolucao de Soma Dobrada

--- src/implementacao/logica/soma-dobrada.js
+++ src/implementacao/logica/soma-dobrada.js
@@ -1,4 +1,12 @@
 function somaDobrada(umValor, outroValor) {
-    return null;
+    if(umValor < 1 || outroValor < 1){
+        return -1
+    }
+    else if(umValor == outroValor){
+        return (umValor + outroValor) * 2
+    }
+    
+    return umValor + outroValor
+    
 }
 module.exports = somaDobrada;
