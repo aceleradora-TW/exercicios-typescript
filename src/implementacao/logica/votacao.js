b3dc3b755346ddee32d58db881c38bacf7945a2c
cintia <cintia.silvadeooliveira@gmail.com>
2022-04-22 11:58:36 -0300

    resolusão exercicio votação

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,32 @@
 function apuraVotacoa(votos) {
-    return null;
+    let contador1=0;
+    let contador2=0;
+    let contador3=0;
+   for (let index = 0; index < votos.length; index++) {
+       const voto = votos[index];
+       if (voto===1){
+        contador1 ++;
+       }
+       if (voto===2){
+        contador2 ++;
+       }
+       if (voto===3){
+        contador3 ++;
+       }
+    }
+    if (contador1 > contador2 && contador1 > contador3){
+        return "Vencedor: 1";
+    }
+    if (contador2 > contador1 && contador2 > contador3){
+        return "Vencedor: 2";
+    }
+    if (contador3 > contador1 && contador3 > contador2){
+        return "Vencedor: 3";
+    }
+    else {
+        return "Empate"
+    }
+    
 }
+
 module.exports = apuraVotacoa;
