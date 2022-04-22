98a267226980bed70d111ed915c5a72169cbbe96
Leticia <leticianunesq8@gmail.com>
2022-04-22 15:53:58 -0300

    Resolucao de numeros-armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,25 @@
 function numerosArmstrong(numero) {
-    return null;
+   var charNum = String(numero)
+
+   var arrayNum = []
+   var cont = 0
+
+   for(var i=0; i < charNum.length; i++){
+    var elevacao = charNum[i] ** charNum.length
+    arrayNum.push(elevacao)
+   }
+
+   for(var i=0; i < arrayNum.length; i++){
+       cont = cont + arrayNum[i]
+   }
+
+   //var resultado = (cont == numero) ? 'Este nao eh um numero de Armstrong!' : 'Este eh um numero de Armstrong!'
+
+   if(cont == numero){
+       return 'Este eh um numero de Armstrong!'
+   }
+   return 'Este nao eh um numero de Armstrong!'
+
+
 }
 module.exports = numerosArmstrong;
