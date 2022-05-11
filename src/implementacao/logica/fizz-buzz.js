0f832d42130725ca0bc7a7c8b4605805bee00cdc
ludmila <ludmilakdk@gmail.com>
2022-05-11 11:57:56 -0300

    resolucao exercicio fizzbuzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,4 +1,14 @@
 function fizzBuzz(numero) {
-    return null;
+    
+    if(numero % 3 == 0 && numero % 5 == 0) {
+        return "FizzBuzz";
+    } else if(numero % 3 == 0){
+        return "Fizz";
+    } else if(numero % 5 == 0) {
+        return "Buzz";
+    } else{
+        return numero.toString();
+    } 
+    
 }
 module.exports = fizzBuzz;
