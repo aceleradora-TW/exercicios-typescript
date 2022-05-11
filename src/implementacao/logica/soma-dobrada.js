d2cd130881e592e93259e7c7e9710623024c7ffe
ludmila <ludmilakdk@gmail.com>
2022-05-11 13:26:33 -0300

    resolucao do exercicio soma-dobrada

--- src/implementacao/logica/soma-dobrada.js
+++ src/implementacao/logica/soma-dobrada.js
@@ -1,4 +1,11 @@
 function somaDobrada(umValor, outroValor) {
-    return null;
+    
+    if(umValor == outroValor){
+        return 2*(umValor+outroValor);
+    } else if(umValor < 1 || outroValor < 1) {
+        return -1;
+    } else {
+        return umValor + outroValor;
+    }
 }
 module.exports = somaDobrada;
