a70bde7142d46dbc7993e99efade1762bd4bbc36
ludmila <ludmilakdk@gmail.com>
2022-05-12 15:57:24 -0300

    resolucao do exercicio objeto-data

--- src/implementacao/logica/objeto-data.js
+++ src/implementacao/logica/objeto-data.js
@@ -1,4 +1,10 @@
 function criaObjeto(data) {
-    return null;
+    
+    return {
+        dia: data[0][1],
+        mes: data[3][4],
+        ano: data[6][7][8][9]
+    }
+    
 }
 module.exports = criaObjeto;
