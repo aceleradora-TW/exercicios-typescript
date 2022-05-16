1a05f718f020261328840a5b9424db07dc3681fb
ludmila <ludmilakdk@gmail.com>
2022-05-16 14:43:00 -0300

    resolucao do exercicio numeros-armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,24 @@
 function numerosArmstrong(numero) {
-    return null;
+
+    let inteiroPraString = numero.toString()    // parametro vira string    
+    let numeroDigitos = inteiroPraString.length // potencia
+    let digitoNumero = 0; // cada digito do parametro
+    let somaDigitos = 0;
+
+
+    for(let i = 0; i < numeroDigitos; i++){
+
+        digitoNumero = (inteiroPraString[i] ** numeroDigitos)
+        somaDigitos += digitoNumero
+
+    }
+
+    // diz se é ou não numero de armstrong
+    if(somaDigitos == numero){
+        return "Este eh um numero de Armstrong!"
+    }
+        return "Este nao eh um numero de Armstrong!"
+
+    
 }
 module.exports = numerosArmstrong;
