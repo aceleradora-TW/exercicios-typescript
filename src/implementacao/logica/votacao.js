cc9309f08abc0c8a08e25c74be11a928f90ca6e8
ludmila <ludmilakdk@gmail.com>
2022-05-16 13:45:11 -0300

    resolucao do exercicio votacao

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,27 @@
 function apuraVotacoa(votos) {
-    return null;
+
+    let contaVoto1 = 0
+    let contaVoto2 = 0
+    let contaVoto3 = 0
+
+    for(let i = 0; i <= votos.length; i++){
+        if(votos[i] == 1){
+            contaVoto1 += 1;
+        } else if (votos[i] == 2){
+            contaVoto2 += 1;
+        } else if(votos[i] == 3){
+            contaVoto3 += 1;
+        }
+    }
+
+    if(contaVoto1 > contaVoto2 && contaVoto1 > contaVoto3){
+        return "Vencedor: 1";
+    } else if(contaVoto2 > contaVoto1 && contaVoto2 > contaVoto3){
+        return "Vencedor: 2";
+    } else if(contaVoto3 > contaVoto2 && contaVoto3 > contaVoto1){
+        return "Vencedor: 3";
+    }
+        return "Empate";
+    
 }
 module.exports = apuraVotacoa;
