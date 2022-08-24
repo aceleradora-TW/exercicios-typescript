96dc021ef7b9ba1330c0539f401d0584aaf3ee10
Enzo Soares <soaressenzo@gmail.com>
2022-08-24 14:27:16 -0300

    Realizada a implementacao da questao numeros-armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,21 @@
 function numerosArmstrong(numero) {
-    return null;
+    let soma=0;
+    let numeroString = numero.toString();
+    if(numero > 10){
+        for(let i = 0; i<numeroString.length; i++){
+            soma += Math.pow(parseFloat(numeroString[i]),numeroString.length);
+        }
+        if(soma == numero){
+            return 'Este eh um numero de Armstrong!';
+        }else{
+            return 'Este nao eh um numero de Armstrong!';
+        }
+    }
+    soma = Math.pow(numero,numeroString.length);
+    if(soma == numero){
+        return 'Este eh um numero de Armstrong!';
+    }else{
+        return 'Este nao eh um numero de Armstrong!';
+    }
 }
 module.exports = numerosArmstrong;
