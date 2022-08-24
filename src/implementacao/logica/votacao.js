234a1ae5df0ada9e4a84cea6ac2fe130f97e4568
Enzo Soares <soaressenzo@gmail.com>
2022-08-24 13:48:10 -0300

    Implementando a questao da votacao

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,30 @@
 function apuraVotacao(votos) {
-    return null;
+    let countNum1, countNum2, countNum3;
+    countNum1 = countNum2 = countNum3 =0;
+
+    for(let i =0; i<votos.length; i++){
+        if(votos[i] == 1){
+            countNum1 ++;
+        }else if(votos[i]==2){
+            countNum2 ++;
+        }else if(votos[i]== 3){
+            countNum3 ++;
+        }
+    }
+    if(countNum1 > countNum2){
+        if(countNum1 > countNum3){
+            return "Vencedor: 1";
+        }else{
+            return "Vencedor: 3";
+        }
+    }
+    else{
+        if(countNum2> countNum3){
+            return "Vencedor: 2";
+        }else{
+            return "Vencedor: 3";
+        }
+    }
+    return "Empate";
 }
 module.exports = apuraVotacao;
