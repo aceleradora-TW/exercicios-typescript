2dc57e1c0a86657480404ffae4309f243b5da133
Enzo Soares <soaressenzo@gmail.com>
2022-08-24 11:40:38 -0300

    Realizada a implementacao do exercicio soma-dobrada

--- src/implementacao/logica/soma-dobrada.js
+++ src/implementacao/logica/soma-dobrada.js
@@ -1,4 +1,9 @@
 function somaDobrada(umValor, outroValor) {
-    return null;
+    if(umValor == outroValor){
+        return (umValor+outroValor)*2;
+    }else if(umValor<1 || outroValor<1){
+        return -1;
+    }
+    return umValor+outroValor;
 }
 module.exports = somaDobrada;
