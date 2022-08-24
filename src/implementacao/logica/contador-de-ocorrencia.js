2f7a19922fdfff90964dd374affeede788e52961
Enzo Soares <soaressenzo@gmail.com>
2022-08-24 11:48:43 -0300

    Realizada a implementacao da atividade contador-de-ocorrencia

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,4 +1,11 @@
 function contadorDeOcorrencia(texto, letra) {
-    return null;
+    let count = 0;
+    for(let i =0;i<texto.length;i++){
+        if(texto[i]== letra){
+            count ++;
+        }
+    }
+    
+    return count;
 }
 module.exports = contadorDeOcorrencia;
