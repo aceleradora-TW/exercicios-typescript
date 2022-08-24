3a9e1c18bf3fb91151c3cfd100502e1838541263
Enzo Soares <soaressenzo@gmail.com>
2022-08-24 11:27:13 -0300

    Realizado a implementacao dos exercicios Hello World e FizzBuzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,4 +1,11 @@
 function fizzBuzz(numero) {
-    return null;
+    if(numero % 3 == 0 && numero % 5 ==0){
+        return 'FizzBuzz';
+    }else if(numero % 5 == 0){
+        return 'Buzz';
+    }else if(numero % 3 == 0){
+        return 'Fizz';
+    }
+    return numero.toFixed(0);
 }
 module.exports = fizzBuzz;
