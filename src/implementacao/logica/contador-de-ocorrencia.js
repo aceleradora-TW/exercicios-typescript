dc5b378a9f22747b8a37c49dab59273521f7a9f6
Vanessa <vanessa.almirante@hotmail.com>
2022-09-12 14:42:33 -0300

    contador-de-ocorrencia

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,4 +1,10 @@
 function contadorDeOcorrencia(texto, letra) {
-    return null;
+    let cont = 0;
+    const tam = texto.length;
+
+    for(let i = 0; i<tam; i++){
+        if(texto[i]=== letra)cont++;
+    }  return cont;
+     
 }
 module.exports = contadorDeOcorrencia;
