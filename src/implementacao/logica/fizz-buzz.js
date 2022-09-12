327818a25b166e46695770e1308bd3f4654d55bd
Jean Castilhos <jccastilhos182@gmail.com>
2022-09-12 11:03:36 -0300

    fisrt commit

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,4 +1,12 @@
 function fizzBuzz(numero) {
+    
+    const num = numero;
+
+    if (numero % 2 === 0) {
+        console.log("O numero eh divisivel por 2");
+    } else {
+        console.log("O numero nao eh divisivel por 2");
+    }
     return null;
 }
 module.exports = fizzBuzz;
