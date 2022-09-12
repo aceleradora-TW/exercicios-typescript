5e907b703337dfdd0eb7604d7efa3ff5d381ab3d
Jean Castilhos <jccastilhos182@gmail.com>
2022-09-12 15:23:26 -0300

    add objteo_data

--- src/implementacao/logica/objeto-data.js
+++ src/implementacao/logica/objeto-data.js
@@ -1,4 +1,10 @@
 function criaObjeto(data) {
-    return null;
+    const obj_data = {
+        dia: `${data[0]}${data[1]}`,
+        mes: `${data[3]}${data[4]}`,
+        ano: `${data[6]}${data[7]}${data[8]}${data[9]}`
+    };
+
+    return obj_data;
 }
 module.exports = criaObjeto;
