bb6f3ec16d6d7a30db8ff675524a37dfd44e30af
Tales Félix <tales.felix.10@gmail.com>
2022-09-12 13:31:33 -0300

    add soma-dobrada

--- src/implementacao/logica/soma-dobrada.js
+++ src/implementacao/logica/soma-dobrada.js
@@ -1,4 +1,9 @@
 function somaDobrada(umValor, outroValor) {
-    return null;
+
+    if(umValor<1 || outroValor<1) return -1
+
+    if(umValor===outroValor) return umValor*4
+    
+    return umValor+outroValor;
 }
 module.exports = somaDobrada;
