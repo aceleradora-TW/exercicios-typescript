9894cc57f49cc190830e24f4322eb5d2d78f16b5
Vanessa <vanessa.almirante@hotmail.com>
2022-09-13 16:06:05 -0300

    add numeros-armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,17 @@
 function numerosArmstrong(numero) {
-    return null;
+    const algorismo = (numero + '').length;
+    const vetor = (numero+'').split('');
+    let soma = 0;
+
+    vetor.forEach((num) => { 
+    soma += Math.pow(num, algorismo);
+    });
+
+    if(soma === numero)return "Este eh um numero de Armstrong!";
+
+    return "Este nao eh um numero de Armstrong!"
+
+
+    
 }
 module.exports = numerosArmstrong;
