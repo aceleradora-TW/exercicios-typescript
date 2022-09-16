34a937a292c9a3847e21bccb0f2dc858a6efa6aa
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-09-16 11:54:36 -0300

    Resolução do exercício Contador de ocorrências

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,4 +1,24 @@
 function contadorDeOcorrencia(texto, letra) {
-    return null;
+    let count = 0
+
+    for (i = 0; i <= texto.length; i++) {
+        if (texto[i] === letra) {
+            count++
+
+        }
+    }
+    return count
+    
 }
+
+console.log(contadorDeOcorrencia("bolo", "o"))
+/*if (texto, letra) {
+        return 2
+}
+if (texto, letra) {
+    return 3
+}
+if (texto, letra) {
+    return 1
+}*/
 module.exports = contadorDeOcorrencia;
