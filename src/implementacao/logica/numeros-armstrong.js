ca7d3e3929f67e0fad64830de344d5bf24563032
Guilherme-Soares777 <guilhermesoaresavilars@gmail.com>
2022-09-23 13:10:11 -0300

    Resolução dos exercícios de lógica de programação

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,19 @@
 function numerosArmstrong(numero) {
-    return null;
+    let numSaveArray = numero.toString().split('')
+    let soma = 0
+    for (i = 0; i < numSaveArray.length; i++) {
+        let pontecia = numSaveArray[i] ** numSaveArray.length
+        soma += pontecia
+
+    }
+    if (soma === numero) {
+        return 'Este eh um numero de Armstrong!'
+    } else if (soma !== numero) {
+        return 'Este nao eh um numero de Armstrong!'
+    }
+
+
+
 }
+
 module.exports = numerosArmstrong;
