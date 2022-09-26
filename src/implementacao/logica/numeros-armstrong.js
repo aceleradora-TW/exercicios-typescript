25014727d2db19fe54214db2aa7f758c68811612
Gabriela_gomes3108 <gabrieladepaula8568@gmail.com>
2022-09-26 11:47:50 -0300

    Meu repositorio

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,15 @@
 function numerosArmstrong(numero) {
-    return null;
+    let guardar = numero.toString().split("")
+    let conta = 0
+    for (i = 0; i < guardar.length; i++) {
+        let potencia = guardar[i] ** guardar.length
+        conta += potencia
+    }
+    if (conta === numero) {
+        return "Este eh um numero de Armstrong!"
+    }
+    return 'Este nao eh um numero de Armstrong!'
 }
+
+numerosArmstrong(153)
 module.exports = numerosArmstrong;
