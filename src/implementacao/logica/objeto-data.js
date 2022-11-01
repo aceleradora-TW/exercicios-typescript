9e063e6c87d2bec4568b4445d837a8ed1d0d27ea
Keu Santos <modulodigital69@gmail.com>
2022-11-01 14:21:28 -0300

    objeto data

--- src/implementacao/logica/objeto-data.js
+++ src/implementacao/logica/objeto-data.js
@@ -1,4 +1,11 @@
 function criaObjeto(data) {
-    return null;
+    let novaData= data.split("/",3)
+ 
+    const objData = {
+        dia:novaData[0],
+        mes:novaData[1],
+        ano:novaData[2]
+    }
+    return objData; 
 }
 module.exports = criaObjeto;
