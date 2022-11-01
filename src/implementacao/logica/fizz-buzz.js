623e13c7e78fe827366e0014deacd88fce8bfe73
Keu Santos <modulodigital69@gmail.com>
2022-11-01 14:20:54 -0300

    fizBuzz

--- src/implementacao/logica/fizz-buzz.js
+++ src/implementacao/logica/fizz-buzz.js
@@ -1,4 +1,13 @@
 function fizzBuzz(numero) {
-    return null;
+    if((numero % 3 === 0) && (numero % 5 ===0)){
+        return "FizzBuzz"
+    }if(numero % 3 === 0){
+        return "Fizz"
+    }if (numero % 5 ===0){
+        return "Buzz"
+    } else{
+        let numeroString= numero.toString()
+        return numeroString
+    }
 }
 module.exports = fizzBuzz;
