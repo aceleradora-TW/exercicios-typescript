90110d1ff9d889eab7e2b0270e46162c56cb5d25
Keu Santos <modulodigital69@gmail.com>
2022-11-01 14:25:03 -0300

    resolvendo coflict

--- src/implementacao/logica/contador-de-ocorrencia.js
+++ src/implementacao/logica/contador-de-ocorrencia.js
@@ -1,9 +1,4 @@
 function contadorDeOcorrencia(texto, letra) {
-<<<<<<< HEAD
     return texto.split(letra).length - 1;
-=======
-    return texto.split(letra).length - 1
-   
->>>>>>> 420bd75f9062e439851813fc933347e50b6df7f8
 }
 module.exports = contadorDeOcorrencia;
