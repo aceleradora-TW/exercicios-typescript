6c8654774618f53e4e21261a1e1f1e971ccff285
Keu Santos <modulodigital69@gmail.com>
2022-11-01 14:22:31 -0300

    votaçao

--- src/implementacao/logica/votacao.js
+++ src/implementacao/logica/votacao.js
@@ -1,4 +1,32 @@
 function apuraVotacao(votos) {
-    return null;
+  let contagemDeVotos = 0;
+  let contagemDeVotos2 = 0;
+  let contagemDeVotos3 = 0;
+
+  for (i = 0; i < votos.length; i++) {
+    if (votos[i] === 1) {
+      contagemDeVotos++;
+    }
+    if (votos[i] === 2) {
+      contagemDeVotos2++;
+    }
+    if (votos[i] === 3) {
+      contagemDeVotos3++;
+    }
+  }
+  const vencedores = [
+    [contagemDeVotos, "Vencedor: 1"],[contagemDeVotos2, "Vencedor: 2"],[contagemDeVotos3, "Vencedor: 3"]
+  ];
+
+  vencedores.sort((prev, next) => {
+    return prev[0] - next[0];
+  });
+
+  if (vencedores[1][0] === vencedores[2][0]) {
+    return "Empate";
+  }
+
+  return vencedores[2][1];
 }
+
 module.exports = apuraVotacao;
