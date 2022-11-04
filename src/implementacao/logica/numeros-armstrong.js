cff7c50105bae2187c056721ad988219649b7686
Keu Santos <modulodigital69@gmail.com>
2022-11-04 10:27:13 -0300

    resolução do exercicio numero de armstrong

--- src/implementacao/logica/numeros-armstrong.js
+++ src/implementacao/logica/numeros-armstrong.js
@@ -1,4 +1,22 @@
 function numerosArmstrong(numero) {
-    return null;
+  let nEsimaPotência = numero.toString().length;
+  let numeroString = numero.toString();
+  let novoArry = [];
+  let total = 0;
+
+  for (let i = 0; i < numeroString.length; i++) {
+    let calculoExeponecial = Math.pow(numeroString[i], nEsimaPotência);
+    novoArry.push(calculoExeponecial);
+  }
+
+  for (let k = 0; k < novoArry.length; k++) {
+    total += novoArry[k];
+  }
+
+  if (numero === total) {
+    return "Este eh um numero de Armstrong!";
+  } else {
+    return"Este nao eh um numero de Armstrong!";
+  }
 }
 module.exports = numerosArmstrong;
